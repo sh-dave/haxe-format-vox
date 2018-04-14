adb294a7284d3cbf9e38496704f2fe752b758639
David Klein <dave@smokinheads.de>
2018-04-14 10:00:22 +0200

    wip

--- format/vox/Tools.hx
+++ format/vox/Tools.hx
@@ -22,4 +22,24 @@ class Tools {
 		b : (color >> 16) & 0xff,
 		a : (color >> 24) & 0xff,
 	}
+
+	public static function getTranslationFromDict( d: Dict, key: String /*= '_t'*/ ) : { x: Float, y: Float, z: Float } {
+		var t = d.get(key);
+
+		if (t == null) {
+			return { x: 0, y: 0, z: 0 }
+		}
+
+		var split = t.split(' ');
+		var x = Std.parseInt(split[0]);
+		var y = Std.parseInt(split[1]);
+		var z = Std.parseInt(split[2]);
+
+		return { x: x, y: y, z: z }
+	}
+
+	public static function getRotationFromDict( d: Dict, key: String /*= '_r'*/ ) : { x: Float, y: Float, z: Float } {
+		// TODO (DK) implement me
+		return { x: 0, y: 0, z: 0 }
+	}
 }
